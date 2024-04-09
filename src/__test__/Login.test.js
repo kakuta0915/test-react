8b19848f1215cb93d62019c9013e497017a82977
Takeru Kakuta <kakuta19960915@gmail.com>
2024-04-09 21:33:28 +0900

    ✅ test: Emailを間違えて入力したらエラーが出るかのテスト

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react"
-import Login from "../Login"
+import Login, { validateEmail } from "../Login"
 
 describe("Test Login Component", () => {
   test("render form with 1 button", async () => {
     render(<Login />);
-    // eslint-disable-next-line testing-library/await-async-query
     const buttonList = await screen.findAllByRole("button");
     expect(buttonList).toHaveLength(1);
   });
-})
+
+  // validateEmailが正常に動作しているかをテスト (失敗したらtrue)
+  test("should be failed on email validation", () => {
+    const testEmail = "kakuta.com";
+    expect(validateEmail(testEmail)).not.toBe(true);
+  });
+});
